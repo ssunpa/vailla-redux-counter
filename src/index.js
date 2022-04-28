e1ea9ab1b94d3b3f396c1604f8bfbf48eff2016b
ssunpa <bestpa.1130@gmail.com>
2022-04-28 21:25:07 +0900

    counter with pure redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const reset = document.getElementById('reset');
+const number = document.querySelector('span');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+const RESET = 'RESET';
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+    switch (action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        case RESET:
+            return (count = 0);
+        default:
+            return count;
+    }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+    countStore.dispatch({ type: MINUS });
+};
+
+const handelReset = () => {
+    countStore.dispatch({ type: RESET });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
+reset.addEventListener('click', handelReset);
